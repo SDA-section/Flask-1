6d982416c0c2a4461a3cebfdf57daea93b1cfb4d
Mohamed Sayed <mohamedelsayed0901@gmailcom>
2025-02-18 15:01:32 +0200

    Linking Styles and javascript

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Home")
+    return render_template('index.html', title="Home", style='home.css')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title='About', style='about.css')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Contact Us')
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
